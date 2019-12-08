748d104284f196ee385a3f541be8cc60ea7bdf12
SiePoma <silviopoma91@gmail.com>
2019-12-08 20:28:58 +0100

    migliorato bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sudo yum install epel-release
-sudo yum install python-pip
-sudo yum install python-devel
-sudo yum groupinstall 'development tools'
+sudo yum install -y epel-release
+sudo yum install -y python-pip
+sudo yum install -y python-devel
+sudo yum groupinstall -y 'development tools'
